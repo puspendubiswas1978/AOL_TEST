7b5d408c1a60da3d6953b6aff9159234f628a655
puspendubiswas1978 <41023251+puspendubiswas1978@users.noreply.github.com>
2018-08-22 09:18:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,109 +1,89 @@
-//"use strict";
-
+const fs = require('fs');
+const readline = require('readline');
 const {google} = require('googleapis');
 
-//const functions = require('firebase-functions');
-//const { WebhookClient } = require('dialogflow-fulfillment');
-//const { Card, Suggestion } = require('dialogflow-fulfillment');
-//process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-let sdata = 'Beginning6';
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_PATH = 'token.json';
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
+});
 
-const restService = express();
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
 
-//const agent = new WebhookClient({ request, response });
-//var jwt = getJwt();
-var apiKey = getApiKey();
-var spreadsheetId = '1y2N9oaj5lYujcR6VaYh3nFI_wxRngRZeAdSnjT3p07o';
-var range = 'Sheet1!A2:E98';
-
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-/*function getJwt()
-{
-    var credentials = require("./credentials.json");
-    return new google.auth.JWT(
-        credentials.client_email, null, credentials.private_key,
-        ['https://www.googleapis.com/auth/spreadsheets']
-    );
-}*/
-
-function getApiKey()
-{
-    var apiKeyFile = require("./api_key.json");
-    return apiKeyFile.key;
-} 
-
-/*function FindSchedule(jwt, apiKey, ssid, rangev)
-{
-    sdata= 'Function called5';
-    const sheets = google.sheets({ version: 'v4' });
-    sheets.spreadsheets.values.get({
-        //sdata : 'Function called3',
-        spreadsheetId: ssid,
-        range: rangev,
-        auth: jwt,
-        key: apiKey
-        //valueInputOption: 'RAW',
-        //resource: { values: [row] }
-    },
-        function (err, result) {
-            //sdata = 'inner function called';
-            if (err) {
-                //throw err;
-                sdata = 'Error2';
-            }
-            else {
-                sdata = 'else part';
-                const rows = result.data.values;
-                //console.log('Updated sheet: ' + result.data.updates.updatedRange);
-                if (rows.length) {
-                    //console.log('Name, Major:');
-                    // Print columns A and C, which correspond to indices 0 and 4.
-                    rows.map((row) => {
-                        //console.log(`${row[0]}, ${row[1]}, ${row[2]}`);
-
-                        if (row[0] == 'Happiness program') {
-                            var currdate = new Date();
-                            sdata = new Date(row[3]).toString(); //currdate.toString();
-                            //if (currdate.getDate() <= row[2])
-                            //if (currdate.compareTo(new Date(row[2])) == -1)
-                            //{
-                            //   sdata = row[4];
-                            return;
-                            //}                                
-                        }
-                    });
-                }
-                else {
-                    //console.log('No data found.');
-                    sdata = 'No data found';
-                }
-            }
-            //return celldata;
-        });
-} */
-
-restService.use(bodyParser.json());
-
-restService.post("/happy", function(req, res)
-{
-  var speech = "Next week in Delhi";
- // FindSchedule(jwt, apiKey, spreadsheetId, range);
-  //speech = sdata;
-  return res.json(
-  {
-    fulfillmentText: speech,
-    source: "webhook-echo-sample"
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
   });
-});
+}
 
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
-});
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1y2N9oaj5lYujcR6VaYh3nFI_wxRngRZeAdSnjT3p07o',
+    range: 'Class Data!A2:E',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name, Major:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      rows.map((row) => {
+        console.log(`${row[0]}, ${row[4]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
